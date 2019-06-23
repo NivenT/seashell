e5ed351ff277c294257249efc24b7c2fc226fb25
NivenT <nachenjang@gmail.com>
2019-06-22 23:41:04 -0700

    prints a prompt (and does nothing else)

---
+++ include/defs.h
@@ -0,0 +1,6 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+#define PROMPT "seashell> "
+
+#endif // DEFS_H_INCLUDED
--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#include "defs.h"
+
 int main(int argc, char *argv[]) {
+  pid_t seashell_pid = getpid();
+  printf(PROMPT);
+  printf("\n");
   return 0;
 }
